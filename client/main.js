3dbd4578c9b59015b7540d3fb1422a03ea673b0b
이우빈 <125638564+dnqls9875@users.noreply.github.com>
2024-10-29 13:39:36 +0900

    core.js 작업환경 구성

---
+++ client/main.js
@@ -0,0 +1,7 @@
+// ! 웹 브라우저에서 읽을 수 있는 환경
+
+// const a = "hello";
+
+function sum() {
+  console.log(1 + 2);
+}
---
+++ server/index.js
@@ -0,0 +1,12 @@
+// ? node 환경에서 읽을 수 있는 환경
+
+const server = require("live-server");
+
+const params = {
+  host: "localhost",
+  port: 3000,
+  open: false,
+  root: "./client",
+};
+
+server.start(params);
