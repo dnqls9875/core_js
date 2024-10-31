f1f99502d47d753e15cf2ec5a7b353f20d6dfddb
이우빈 <125638564+dnqls9875@users.noreply.github.com>
2024-10-31 10:40:35 +0900

    객체, 생성자 함수, this 바인딩

--- client/chapter/core/06.dataType.js
+++ client/chapter/core/06.dataType.js
@@ -13,16 +13,20 @@ let unallocated;
 console.log(typeof unallocated);
 
 // 3. 따옴표를 사용해 묶은 텍스트(큰", 작은', 역`)
-let double = '큰';
+let double = '큰'; // string literal
 let single = '작은';
 let backtic = `인도의 ${double * 30}이 맛있다.`;
 
 console.log(typeof double, typeof single, typeof backtic);
 console.log(backtic);
 
+const str = new String('hello'); // string constructor function (생성자함수)
+
 // 4. 정수, 부동 소수점 숫자(길이 제약)
-let integer = 150;
+let integer = 150; // number literal
 let decimalPoint = 3.14;
+
+const num = new Number(23); // number constructor function (생성자함수)
 
 console.log(typeof integer, typeof decimalPoint);
 
@@ -34,6 +38,8 @@ console.log(typeof bigInt);
 let isActive = true;
 let isFalse = false;
 
+const bool = new Boolean(true); // boolean constructor function (생성자함수)
+
 console.log(typeof isActive);
 console.log(typeof isFalse);
 
@@ -42,6 +48,9 @@ const obj = {
   name: '우빈',
   age: 30,
 };
+
+const obj_ = new Object(); // object constructor function (생성자함수)
+
 console.log(typeof obj);
 
 // 8. 고유한 식별자(unique identifier)
@@ -56,11 +65,70 @@ console.log(typeof unique);
 // 언어 상, 오류
 
 // Object
-typeof [];
-typeof {};
+
+// 객체에 메서드를 정의하는 방법
+
+/**
+ * 1. normal function
+ * 2. arrow function -> 화살표함수는 protype constructor을 내장하지 않음 this를 바인딩
+ * 3. concise method
+ *
+ *
+ */
+
+//  ! 생성자 함수는 무조건 객체를 생성한다. 암기
+//  ?  암묵적으로 생성자함수는 앞글자를 대문자로 작성하고, new를 포함한다.
+const fnc = new Object();
+
+// ? this 나를 호출한 대상
+// ? 메서드 안에서 this를 찾는 경우는 객체 자체를 선택해야 할 때
+// ? arrow function은 this를 바인딩하지 않습니다. (상위 컨텍스트에서 this를 찾습니다.)
+// ? 객체의 메서드를 정의 할 때 => concise method 권장. 이유는 this로 객체 자체로 바인딩 하기 때문에
+// ? 메서드 안에서 함수를 정의해야 할 때 => arrow function 권장. 이유는 this를 찾기 위해서
+
+const user = {
+  name: 'tiger',
+  age: 30,
+  sayHi: function () {
+    // console.log(this) // user
+  },
+  sayHi2: () => {
+    // console.log(this); // window
+  },
+  sayHi3() {
+    // console.log(this); // object (객체)
+
+    function sayBye() {
+      console.log(this);
+    }
+    sayBye(); // window가 호출했으므로 this를 찾으면 window객체가 나온다.
+
+    // 화살표 함수의 this는 객체를 제대로 가져온다.
+    const sayBye2 = () => {
+      console.log((this.age = 30)); // age -> 30
+    };
+    sayBye2();
+  },
+};
+
+// & 생성자 함수
+// % 지속적으로 반복하는 값을 생성할 때 생성자 함수를 사용한다.
+// ^ main constructor
+function User2() {
+  this.payment = false;
+  this.age = 30;
+}
+
+// ? class User2 {} // 요새의 생성자 함수
+
+// ! 인스턴스
+const _a = new User2();
+const _b = new User2();
+const _c = new User2();
+const _d = new User2();
+
 // Array
 
 // function
-console.log(typeof alert);
 
 // this
---
+++ client/chapter/core/test.js
@@ -0,0 +1,6 @@
+let friuits = [2];
+
+friuits.unshift(0, 1);
+friuits.push(3, 4);
+
+console.log(friuits);
