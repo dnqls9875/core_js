0711118c94ed1fcfc89a97fadef08acc61b12080
이우빈 <125638564+dnqls9875@users.noreply.github.com>
2024-11-28 13:02:44 +0900

    TodoList

--- client/main.js
+++ client/main.js
@@ -1,6 +1,8 @@
 import { Button } from './components/Button/Button.js';
 import { Card } from './components/Card/Card.js';
 import { Counter } from './components/Counter/Counter.js';
+import { TodoList } from './components/TodoList/TodoList.js';
+import { TodoItem } from './components/TodoItem/TodoItem.js';
 
 const app = document.getElementById('app');
 
@@ -8,10 +10,13 @@ function defineElement() {
   customElements.define('custom-button', Button);
   customElements.define('custom-card', Card);
   customElements.define('custom-counter', Counter);
+  customElements.define('custom-todo', TodoList);
+  customElements.define('custom-todo-item', TodoItem);
 }
 
 defineElement();
 
-const buttonElement = document.createElement('custom-button');
+const todoElement = document.createElement('custom-todo');
+app.append(todoElement);
 
-app.append(buttonElement);
+// const buttonElement = document.createElement('custom-button');
